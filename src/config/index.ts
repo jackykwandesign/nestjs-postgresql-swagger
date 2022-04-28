007b2fe1573f9e6b7fe047983a99b0d8dc1502a1
jackykwandesign <jackykwandesign@gmail.com>
2022-04-28 14:52:59 +0000

    add source

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
-// src/config/config.service.ts
+// https://medium.com/@gausmann.simon/nestjs-typeorm-and-postgresql-full-example-development-and-project-setup-working-with-database-c1a2b1b11b8f
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
